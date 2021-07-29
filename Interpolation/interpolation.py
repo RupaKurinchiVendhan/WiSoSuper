17253b091eac994211d72a49a68607afb8acae51
rupakv0827 <rkurinchi-vendhan@gmail.com>
2021-07-29 12:45:37 -0400

    added interpolation

--- Interpolation/interpolation.py
+++ Interpolation/interpolation.py
@@ -4,7 +4,8 @@ import numpy as np
 from math import sqrt, floor, log10
 from skimage.metrics import structural_similarity as ssim
 from PIL import Image
-
+import pandas as pd
+from comparison.metrics import *
 
 def read_image(path):
     '''Read image and return the image propertis.
@@ -21,7 +22,6 @@ def read_image(path):
     dimension = (size[0], size[1])
 
     return img, size, dimension
-
 
 def image_change_scale(img, dimension, scale=100, interpolation=cv2.INTER_LINEAR):
     '''Resize image to a specificall scale of original image.
@@ -104,107 +104,6 @@ def bilinear_interpolation(image, dimension):
     return new_image
 
 
-def u(s, a): 
-    if (abs(s) >= 0) & (abs(s) <= 1): 
-        return (a+2)*(abs(s)**3)-(a+3)*(abs(s)**2)+1
-    elif (abs(s) > 1) & (abs(s) <= 2): 
-        return a*(abs(s)**3)-(5*a)*(abs(s)**2)+(8*a)*abs(s)-4*a 
-    return 0
-  
-  
-# Padding 
-def padding(img, H, W, C): 
-    zimg = np.zeros((H+4, W+4, C)) 
-    zimg[2:H+2, 2:W+2, :C] = img 
-      
-    # Pad the first/last two col and row 
-    zimg[2:H+2, 0:2, :C] = img[:, 0:1, :C] 
-    zimg[H+2:H+4, 2:W+2, :] = img[H-1:H, :, :] 
-    zimg[2:H+2, W+2:W+4, :] = img[:, W-1:W, :] 
-    zimg[0:2, 2:W+2, :C] = img[0:1, :, :C] 
-      
-    # Pad the missing eight points 
-    zimg[0:2, 0:2, :C] = img[0, 0, :C] 
-    zimg[H+2:H+4, 0:2, :C] = img[H-1, 0, :C] 
-    zimg[H+2:H+4, W+2:W+4, :C] = img[H-1, W-1, :C] 
-    zimg[0:2, W+2:W+4, :C] = img[0, W-1, :C] 
-    return zimg 
-  
-  
-# Bicubic operation 
-def bicubic(img, ratio, a): 
-    
-    # Get image size 
-    H, W, C = img.shape 
-      
-    # Here H = Height, W = weight, 
-    # C = Number of channels if the  
-    # image is coloured. 
-    img = padding(img, H, W, C) 
-      
-    # Create new image 
-    dH = floor(H*ratio) 
-    dW = floor(W*ratio) 
-  
-    # Converting into matrix 
-    dst = np.zeros((dH, dW, 3))   
-    # np.zeroes generates a matrix  
-    # consisting only of zeroes 
-    # Here we initialize our answer  
-    # (dst) as zero 
-  
-    h = 1/ratio 
-  
-    print('Start bicubic interpolation') 
-    print('It will take a little while...') 
-    inc = 0
-      
-    for c in range(C): 
-        for j in range(dH): 
-            for i in range(dW): 
-                
-                # Getting the coordinates of the 
-                # nearby values 
-                x, y = i * h + 2, j * h + 2
-  
-                x1 = 1 + x - floor(x) 
-                x2 = x - floor(x) 
-                x3 = floor(x) + 1 - x 
-                x4 = floor(x) + 2 - x 
-  
-                y1 = 1 + y - floor(y) 
-                y2 = y - floor(y) 
-                y3 = floor(y) + 1 - y 
-                y4 = floor(y) + 2 - y 
-                  
-                # Considering all nearby 16 values 
-                mat_l = np.matrix([[u(x1, a), u(x2, a), u(x3, a), u(x4, a)]]) 
-                mat_m = np.matrix([[img[int(y-y1), int(x-x1), c], 
-                                    img[int(y-y2), int(x-x1), c], 
-                                    img[int(y+y3), int(x-x1), c], 
-                                    img[int(y+y4), int(x-x1), c]], 
-                                   [img[int(y-y1), int(x-x2), c], 
-                                    img[int(y-y2), int(x-x2), c], 
-                                    img[int(y+y3), int(x-x2), c], 
-                                    img[int(y+y4), int(x-x2), c]], 
-                                   [img[int(y-y1), int(x+x3), c], 
-                                    img[int(y-y2), int(x+x3), c], 
-                                    img[int(y+y3), int(x+x3), c], 
-                                    img[int(y+y4), int(x+x3), c]], 
-                                   [img[int(y-y1), int(x+x4), c], 
-                                    img[int(y-y2), int(x+x4), c], 
-                                    img[int(y+y3), int(x+x4), c], 
-                                    img[int(y+y4), int(x+x4), c]]]) 
-                mat_r = np.matrix( 
-                    [[u(y1, a)], [u(y2, a)], [u(y3, a)], [u(y4, a)]]) 
-                  
-                # Here the dot function is used to get  
-                # the dot product of 2 matrices 
-                dst[j, i, c] = np.dot(np.dot(mat_l, mat_m), mat_r) 
-
-    return dst 
-
-
 def bicubic_interpolation(img, dimension):
     '''Bicubic interpolation method to convert small size image to original size image
     Parameters:
@@ -241,36 +140,6 @@ def bicubic_interpolation(img, dimension):
                 output[i, j, c] = np.clip(out, 0, 255)
 
     return output
-
-
-def psnr(imageA, imageB):
-    mse = np.mean((imageA - imageB) ** 2)
-    # MSE is zero means no noise is present in the signal and PSNR has no importance.
-    if(mse == 0):  
-        return 100
-    max_pixel = 255.0
-    psnr = 20 * log10(max_pixel / sqrt(mse))
-    return psnr
-    
-
-def mse(imageA, imageB):
- # the 'Mean Squared Error' between the two images is the sum of the squared difference between the two images
- mse_error = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
- mse_error /= float(imageA.shape[0] * imageA.shape[1])
- # return the MSE. The lower the error, the more "similar" the two images are.
- return mse_error
-
-
-def mae(imageA, imageB):
-
-    mae = np.sum(np.absolute((imageB.astype("float") - imageA.astype("float"))))
-    mae /= float(imageA.shape[0] * imageA.shape[1] * 255) 
-
-    if (mae < 0):
-        return mae * -1
-    else:
-        return mae
-
 
 def main(filename):
     images_list = {}
@@ -355,12 +224,3 @@ def main(filename):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return np.array(bil_img_algo), np.array(cubic_img_algo)
-
-
-if __name__ == "__main__":
-    # main("interp_ua_1.png")
-    # main("interp_ua_2.png")
-
-    # main("interp_va_1.png")
-    # main("interp_va_2.png")
-    main("wind paper real.png")
