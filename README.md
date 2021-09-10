a596acc3aa92849b86920be07c5dddaab854ce53
rupakv0827 <rkurinchi-vendhan@gmail.com>
2021-09-10 12:37:24 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 # Evaluating Spatial Super-Resolution Models for National Wind and Solar Data
 <div align="center">
-## Caltech SURF Project for 2020-2021
-## Rupa Kurinchi-Vendhan
-### Mentors: Lucien Werner, Björn Lütjens, Ritwik Gupta, Steven Low
+Caltech SURF Project for 2020-2021
+
+Rupa Kurinchi-Vendhan, Lucien Werner, Björn Lütjens, Ritwik Gupta
 
 ## Abstract
 As the United States constructs additional renewable wind and solar energy power plants, policy makers in charge of operational decision making, scheduling, and resource allocation are faced with challenges introduced by the variability in spatial resolution in solar irradiance and wind speeds. Physics-based short-term forecasting models predict wind speeds and solar irradiance fields at coarse resolutions. Thus, machine learning-based super-resolution methods have been developed to provide higher fidelity for decision making. We present a benchmark of wind and solar data super-resolution methods. In addition to simple interpolation methods, we investigate three machine learning methods: the state-of-the-art physics-informed resolution-enhancing generative adversarial network (PhIREGAN) model, an enhanced deep super-resolution (EDSR) network, and the enhanced super-resolution generative adversarial network (ESRGAN). Additionally, we implement and apply standardized evaluation metrics and climatological data analysis tools which enable an in-depth comparison of simple interpolation techniques and deep learning SR models.
+
+
+## Dataset
+The dataset used for this project is available through the "data.ipynb" notebook. This file also contains instructions for generating your own machine learning-ready dataset, with flexibility to change the parameters inputted to NREL's WIND Toolkit and NSRDB.
+
+## Training
+To train the PhIREGAN, ESRGAN, or EDSR models to achieve 5x super-resolution (SR), use the following commands.
+
+**PhIREGAN**
+`main.py --model=phiregan --mode=train --data_dir=path/to/train/data --data_type=data_type`
+
+Here, `data` should be a folder of TFRecords. The `data_type` can either be `wind` or `solar`. To train the SR CNN, replace `phiregan` with `srcnn`. 
+
+**ESRGAN**
+`main.py --model=esrgan --mode=train --data_dir=path/to/train/data`
+
+As opposed to the PhIREGAN, the data directories for EDSR and ESRGAN must be a folder with HR and LR subdirectories of images. 
+
+**EDSR**
+`main.py --model=edsr --mode=train --data_dir=path/to/train/data --valid_dir=path/to/valid/data --cuda=0`
+
+## Testing
+Once you have trained your models, use the commands below to run them on test data.
+
+**PhIREGAN**
+`main.py --model=phiregan --mode=test --data_dir=path/to/test/data --data_type=data_type`
+
+To test the SR CNN, replace `phiregan` with `srcnn`.
+
+**ESRGAN**
+`main.py --model=esrgan --mode=test --data_dir=path/to/test/data --save_dir=path/to/save`
+
+Here, `save_dir` is the directory where SR outputs should be saved.
+
+**EDSR**
+`main.py --model=edsr --mode=test --data_dir=path/to/test/data --save_dir=path/to/save --model_path=path/to/model --cuda=0`
--- main.py
+++ main.py
@@ -44,13 +44,14 @@ def phiregan(data_type, data_dir, mode):
                                         HR_data_path=HR_data_path,
                                         model_path=model_dir,
                                         batch_size=1)
-        phiregans.test(r=r,
-                data_path=LR_data_path,
-                model_path=model_dir,
-                data_type=data_type,
-                model_type='phiregan',
-                timestep=timestep, 
-                batch_size=1)
+        if mode == 'test':
+            phiregans.test(r=r,
+                    data_path=LR_data_path,
+                    model_path=model_path,
+                    data_type=data_type,
+                    model_type='phiregan',
+                    timestep=timestep, 
+                    batch_size=1)
 
 def srcnn(data_type, data_dir, mode):
     for filename in os.listdir(data_dir):
@@ -84,29 +85,33 @@ def srcnn(data_type, data_dir, mode):
                                         HR_data_path=HR_data_path,
                                         model_path=model_dir,
                                         batch_size=1)
-        sr_data = phiregans.test(r=r,
+        if mode == 'test':
+            phiregans.test(r=r,
                     data_path=LR_data_path,
                     model_path=model_dir,
                     batch_size=1, 
                     data_type=data_type,
-                    model_type='phiregan',
+                    model_type='cnn',
                     timestep=timestep) 
         
 
-def edsr(model_path, data_dir, valid_dir, save_dir, ext, mode, resume=None, cuda=None):
+def edsr(mode, data_dir=None, ext='.png', valid_dir=None, save_dir=None, model_path=None, resume=None, cuda=None):
     if mode == 'train':
         train(data_dir, valid_dir, ext, ext, resume=resume, cuda=cuda)
-    global sess
-    sess = tf.Session()
-    model = get_generator('edsr', is_train=False)
-    print("Loading model")
-    model.load_weights(model_path)
-    sr_from_folder(model, valid_dir+'/LR', save_dir, ext)
+    if mode == 'test':
+        global sess
+        sess = tf.Session()
+        model = get_generator('edsr', is_train=False)
+        print("Loading model")
+        model.load_weights(model_path)
+        sr_from_folder(model, data_dir+'/LR', save_dir, ext)
 
-def esrgan(data_dir, valid_dir, save_dir, mode, pretrain_generator=True):
+
+def esrgan(mode, data_dir, save_dir, pretrain_generator=True):
     if mode == 'train':
         pretrain(data_dir=data_dir, pretrain_generator=pretrain_generator)
-    test(valid_dir=valid_dir, save_dir=save_dir)
+    if mode == 'test':
+        test(valid_dir=data_dir, save_dir=save_dir)
     
 
 def main():
@@ -116,7 +121,7 @@ def main():
     parser.add_argument('--mode', required=True, type=str, help='Train or test')    
     parser.add_argument('--data_type', required=True, type=str, help='Wind or solar')
     parser.add_argument('--data_dir', type=str, default=None, help='Path to training images/tfrecords')
-    parser.add_argument('--valid_dir', type=str, required=False, help='Path to test images/tfrecords')
+    parser.add_argument('--valid_dir', type=str, default=None, help='Path to validation images/tfrecords')
     parser.add_argument('--ext', type=str, default='.png', help='Image extension')
     parser.add_argument('--save_dir', type=str, required=False, help='Folder to save SR images')    
     parser.add_argument('--resume', type=str, default=None, help='Path to checkpoint file')
@@ -131,10 +136,9 @@ def main():
     elif (args.model == 'srcnn'):
         srcnn(args.data_type, args.data_dir, args.mode)
     elif (args.model == 'edsr'):
-        edsr(args.model_path, args.data_dir, args.valid_dir, args.save_dir, args.ext, args.mode, args.resume, args.cuda)
+        edsr(args.mode, args.data_dir, args.ext, args.valid_dir, args.save_dir, args.model_path, args.resume, args.cuda)
     elif (args.model == 'esrgan'):
-        esrgan(args.data_dir, args.valid_dir, args.save_dir, args.mode)
-
+        esrgan(args.mode, args.data_dir, args.save_dir)
 
 if __name__ == '__main__':
     main()
